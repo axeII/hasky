b2d65742474940e33f66b0e7708be3bc78a74fef
axeII <ales.lerch@gmail.com>
2017-12-26 18:38:21 +0100

    eddit ast, code clean

--- ast.py
+++ ast.py
@@ -4,40 +4,6 @@ AST tree for data sctructure of grammar data
 """
 
 __author__ = 'ales lerch'
-"""
-class Node:
-
-    def __init__(self, name, predict, data):
-        self.name = name
-        self.data = data
-        self.predicator = predict
-        self.sub_followers = []
-
-class AST:
-
-    def __init__(self):
-        self.root = None
-        self.pointer = None
-
-    def add_node(self, new_n_data, new_key, key):
-        self.pointer = self.search_ast(key)
-        self.pointer.sub_followers.append(Node(new_key, key, new_n_data))#new_data.key
-
-    def get_node(self, key):
-        return self.search_ast(key).data
-
-    def search_ast(self, key):
-        visited = []
-        stack = [self.root]
-        while stack:
-            current = stack.pop()
-            if current.name == key:
-                return current
-            visited.append(current)
-            for next_ in list(filter(lambda x: x not in visited, current.sub_followers)):
-                stack.append(next_)
-        return None #exception? or empty Node?"""
-
 
 class AST:
 
@@ -45,8 +11,14 @@ class AST:
         self.name = name
         self.value = value
 
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __hash__(self):
+        return hash(self.value)
+
     def _eval(self):
-        return self.value.value
+        return self.value.token_value
 
 class Variable(AST):
 

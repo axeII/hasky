4a80348b1a1113ae3397b207e3fdcc29fb02c84d
Ales Lerch <ales.lerch@gmail.com>
2017-12-26 18:39:58 +0100

    fixing lexer, updating main

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+"""main
+
+main module currelty two modes are supported -> interpreter and reading file - executing
+"""
+
+__author__ = 'ales lerch'
+
+from interpret import *
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        Interpret(None, True).interpret()
+    else:
+        Interpret(argv[1], False).interpret()
+

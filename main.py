5ee2623cdc3644aa03a9a515609012af150c85b3
Ales Lerch <ales.lerch@gmail.com>
2017-12-27 02:46:10 +0100

    reafactoring interpret and main

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ main module currelty two modes are supported -> interpreter and reading file - e
 
 __author__ = 'ales lerch'
 
-from interpret import *
 from sys import argv
+from interpret import Interpret
 
 if __name__ == '__main__':
     if len(argv) == 1:
         Interpret(None, True).interpret()
     else:
         Interpret(argv[1], False).interpret()
-
